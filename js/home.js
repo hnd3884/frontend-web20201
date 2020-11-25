3c60271909523281560a76d1cc58cb7d9e03ed70
ngdiho <hoangt2k24@gmail.com>
2020-11-26 00:48:50 +0700

    watch page

--- js/home.js
+++ js/home.js
@@ -1,18 +1,3 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-
 // var slideIndex = 1;
 // showSlides(slideIndex);
 
@@ -38,3 +23,35 @@ function showSlides() {
 //   slides[slideIndex-1].style.display = "block";
 // }
 
+// === carousel ===
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+// === card ===
+var cards = document.getElementsByClassName("card");
+for (i = 0; i < cards.length; i++) {
+  cards[i].setAttribute('onmouseover', 'CardHover(this)');
+  cards[i].setAttribute('onmouseout', 'CardMouseOut(this)');
+}
+
+function CardHover(card){
+  var hover = card.childNodes[3];
+  hover.style.display='block';
+}
+
+function CardMouseOut(card){
+  var hover = card.childNodes[3];
+  hover.style.display='none';
+}
